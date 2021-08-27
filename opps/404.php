c20e5730f6ed9ceee3836b391107d13e11e565a7
MSMak_GitHub <89143494+MSMak-GitHub@users.noreply.github.com>
2021-08-27 21:42:04 +0300

    Add files via upload

--- opps/404.php
+++ opps/404.php
@@ -1,22 +1,20 @@
+<?php
+Header("HTTP/1.1 404 Not Found");
+?>
+
 <!DOCTYPE html>
-<html lang="ru">
 <head>
   <meta charset="UTF-8">
   <title>msmaksite.org</title>
-  <link rel="stylesheet" href="opps/404.css">
 </head>
-<body>
+<body style="background:#c8d5b9;">
   <h1>msmaksite.org</h1>
   <nav class= navai>
     <a href="index.html" href="">Главная</a>
-    <a href="siteob.html">Музыка</a>
-    <a href="games.html">Игры</a>
-    <a href="apps.html">Приложения</a>
-    <div class="animation start-home"></div>
   </nav>
   <br>
   <h2>Ошибка!</h2>
   <h3>Вы попали на несуществующую страницу!</h3>
-<br>
+  <br>
 </body>
 </html>
