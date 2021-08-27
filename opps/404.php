e0ecc2524ae6b4db61bf6295a9b1f300d6a0e860
MSMak_GitHub <89143494+MSMak-GitHub@users.noreply.github.com>
2021-08-27 21:35:06 +0300

    Add files via upload

---
+++ opps/404.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>msmaksite.org</title>
+  <link rel="stylesheet" href="opps/404.css">
+</head>
+<body>
+  <h1>msmaksite.org</h1>
+  <nav class= navai>
+    <a href="index.html" href="">Главная</a>
+    <a href="siteob.html">Музыка</a>
+    <a href="games.html">Игры</a>
+    <a href="apps.html">Приложения</a>
+    <div class="animation start-home"></div>
+  </nav>
+  <br>
+  <h2>Ошибка!</h2>
+  <h3>Вы попали на несуществующую страницу!</h3>
+<br>
+</body>
+</html>
