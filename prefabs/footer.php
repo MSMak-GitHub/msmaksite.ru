9ef223fb7ff70a4e011acc9a3253c56018ab6c52
MSMak_GitHub <89143494+MSMak-GitHub@users.noreply.github.com>
2021-11-28 21:39:10 +0300

    Add files via upload

---
+++ prefabs/footer.php
@@ -0,0 +1,26 @@
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<style>
+    .fteer {
+        padding: 50px;
+    }
+</style>
+
+
+<div class="fteer">
+<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <div class="col-md-4 d-flex align-items-center">
+      <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+        <img src="/favicons/ms-icon-310x310.png" class="bi" width="24" height="24">
+      </a>
+      <span class="text-muted">© 2021 - MS Inc. Все права защищены.</span>
+    </div>
+    </div>
+  </footer>
+  </div>
+
+
+
+
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
---
+++ prefabs/header.php
@@ -0,0 +1,78 @@
+<style>
+    .navbar {
+width: 100%;
+height: 90px;
+box-shadow: 0 4px 10px rgba(0, 0, 0, .1);
+font-family: 'Source Code Pro', monospace;
+}
+.navbar {
+width: 100%;
+height: 90px;
+box-shadow: 0 4px 10px rgba(0, 0, 0, .1);
+font-family: 'Source Code Pro', monospace;
+}
+
+.navbar .container {
+height: inherit;
+display: flex;
+justify-content: space-between;
+align-items: center;
+}
+.navbar-menu {
+list-style-type: none;
+padding-left: 0;
+margin-bottom: 0;
+}
+.navbar-menu li {
+display: inline-block;
+}
+.navbar-menu li a {
+display: inline-block;
+color: #000;
+opacity: 0.7;
+text-decoration: none;
+padding: 10px;
+transition: all .11s ease-in-out;
+}
+.navbar-menu li a:hover {
+opacity: 1;
+}
+.navbar-wrap {
+display: flex;
+flex-flow: row nowrap;
+}
+.navbar-brand {
+font-weight: 700;
+font-size: 26px;
+text-decoration: none;
+color: #000;
+padding: 10px;
+opacity: 7;
+transition: all .11s ease-out;
+}
+.navbar-brand:hover {
+color: darkblue;
+color: #526154;
+}
+</style>
+
+
+<nav class="navbar">
+	<div class="container">
+       <a href="/" class="navbar-brand">MSMAKSITE.RU</a>
+       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+<div class="navbar-wrap">
+       <ul class="navbar-menu">
+       	<li><a href="https://sites.google.com/view/msmaksiteoffical/главная">Новый сайт!</a></li>
+       	<li><a href="/music.php">Музыка</a></li>
+       	<li><a href="/games.php">Игры</a></li>
+       	<li><a href="/apps.php">Приложения</a></li>
+       	<li><a href="/video/index.php">Видео</a></li>
+              <li><a href="/ss.php">Ссылки</a></li>
+       </ul>
+</div>
+	</div>
+</nav>
+<br>
